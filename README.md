9ba7cd3bf418a3d950ad354b38b5c155c4184184
tmq <15077993139@163.com>
2019-11-18 14:45:00 +0800

    修补了上一个框架的的没有vant和px的问题

---
+++ README.md
@@ -0,0 +1,32 @@
+# reportform
+```
+ vue-cli3 + vant（按需引入）+ stylu + rem适配 +vueRouter
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+# vueBaseFrame
+# -
+# vueEcharts
+# reportForm
+# BaseVueFrame-1.0
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view />
+
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o" to='/'>首页</van-tabbar-item>
+      <van-tabbar-item icon="search" to='/about'>发起</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" to='/'>标签</van-tabbar-item>
+    </van-tabbar>
+
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Icon, Tabbar, TabbarItem} from "vant";
+
+@Component({
+  components: {
+    [Icon.name]:Icon,
+    [Tabbar.name]:Tabbar,
+    [TabbarItem.name]:TabbarItem
+  }
+})
+
+export default class Apps extends Vue{
+  active=0
+    // icon= {
+    //   active: 'https://img.yzcdn.cn/vant/user-active.png',
+    //   inactive: 'https://img.yzcdn.cn/vant/user-inactive.png'
+    // }
+
+}
+
+  
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+</style>
